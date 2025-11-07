233308b8068aaa04aa230144e82792582b763578
kondryukov <80988635+kondryukov@users.noreply.github.com>
2025-11-07 11:06:34 +0300

    Update User.java

--- user-service/src/main/java/org/example/domain/User.java
+++ user-service/src/main/java/org/example/domain/User.java
@@ -1,17 +1,15 @@
 package org.example.domain;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-
-import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.util.Date;
 
 @Entity
-
 @Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class User {
     @Id
